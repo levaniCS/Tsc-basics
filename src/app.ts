b11205904bab52a5335a383c5ce4b0cb62824927
levaniCS <levanisarishvili3105@gmail.com>
2020-11-29 21:07:35 +0400

    add comment

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
-// ARRAY (knows what data it stores)
+//! ARRAY (knows what data it stores)
 const names: Array<string> = [];
 names[0].split(' ');
 
-// PROMISE (knows what data it returns)
+//! PROMISE (knows what data it returns)
 const promise = new Promise<string>((resolve, reject) => {
   setTimeout(() => {
     resolve('This is done!')
   }, 2000);
+
+  reject('Something went wrong...')
 })
 
 promise.then(data => data.split(' '))
 
 
+//! BUILD OUR OWN GENERIC TYPE
